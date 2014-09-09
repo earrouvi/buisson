eb426b4b56f0069c560b83e3914a15be402ee1d7
Elsa Arrou-Vignod <earrou-vignod@lucca.fr>
2014-09-09 19:43:37 +0200

    Refonte de la page + style + changement de font + ancres

--- app/scripts/main.js
+++ app/scripts/main.js
@@ -1,30 +1,35 @@
 'use strict';
 
 angular.module('controllers')
-	.controller('MainCtrl', ['$scope', '$http', '$routeParams', function ($scope, $http, $routeParams) {
+	.controller('MainCtrl', ['$scope', '$http', '$routeParams', '$location', '$anchorScroll', function ($scope, $http, $routeParams, $location, $anchorScroll) {
 		$scope.showBlock = function(index) {
 			switch(index) {
 				case 1:
 					$scope.showHome = false;
 					$scope.showWhatHappened = true;
 					$scope.showContact = false;
+					$location.hash('whatHappened');
 					break;
 				case 2:
 					$scope.showHome = false;
 					$scope.showWhatHappened = false;
 					$scope.showContact = true;
+					$location.hash('contact');
 					break;
 				case 0:
 				default:
 					$scope.showHome = true;
 					$scope.showWhatHappened = false;
 					$scope.showContact = false;
+					$location.hash('home');
 					break;
 			}
+			$anchorScroll();
 		};
 
 		$scope.init = function() {
 			$scope.showHome = true;
+			$scope.locationHash = $location.hash();
 		};
 		$scope.init();
 	}]);
